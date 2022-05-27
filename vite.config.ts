f6ef257eb8575c6f7091e33b85d76a6d3c3b9990
wunian <wunian@wunian.site>
2022-05-27 10:51:03 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 配置路径别名
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src'),
+      }
+    ],
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://yourBaseUrl",
+        changeOrigin: true,
+        cookieDomainRewrite: "",
+        secure: false,
+      },
+    },
+  },
+})
